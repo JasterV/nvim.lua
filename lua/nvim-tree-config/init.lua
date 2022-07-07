a314f7803d9846b3b4a63b216aceafb49a82c70b
JasterV <jaster.victor@gmail.com>
2022-07-07 23:57:06 +0200

    adds shade

--- lua/nvim-tree-config/init.lua
+++ lua/nvim-tree-config/init.lua
@@ -1,20 +1,22 @@
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  diagnostics = {
-      enable = true
-  },
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	sort_by = "case_sensitive",
+	diagnostics = {
+		enable = true,
+	},
+	view = {
+		adaptive_size = true,
+		mappings = {
+			list = {
+				{ key = "u", action = "dir_up" },
+			},
+		},
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+	respect_buf_cwd = true,
+	sync_root_with_cwd = true,
 })
--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,31 +1,32 @@
-return require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
-    use 'EdenEast/nightfox.nvim'
-    use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {
-            'kyazdani42/nvim-web-devicons', -- optional, for file icons
-        },
-        tag = 'nightly' -- optional, updated every week. (see issue #1193)
-    }
-	use 'neovim/nvim-lspconfig'
-	use {
-  		'nvim-telescope/telescope.nvim',
-  		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-    use 'L3MON4D3/LuaSnip' -- Snippets plugin
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-    use 'arkav/lualine-lsp-progress'
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
-    use 'nvim-treesitter/nvim-treesitter-refactor'
-    use 'jose-elias-alvarez/null-ls.nvim'
+return require("packer").startup(function()
+	use("wbthomason/packer.nvim")
+	use("EdenEast/nightfox.nvim")
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons", -- optional, for file icons
+		},
+		tag = "nightly", -- optional, updated every week. (see issue #1193)
+	})
+	use("neovim/nvim-lspconfig")
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use("hrsh7th/nvim-cmp") -- Autocompletion plugin
+	use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
+	use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
+	use("L3MON4D3/LuaSnip") -- Snippets plugin
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+	use("arkav/lualine-lsp-progress")
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	use("nvim-treesitter/nvim-treesitter-refactor")
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("sunjon/shade.nvim")
 end)
