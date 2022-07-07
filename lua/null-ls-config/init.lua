e453e12ff1625afdf721fdba3103d0fd9ac69f33
JasterV <jaster.victor@gmail.com>
2022-07-07 21:17:55 +0200

    configures rust & lua formatters

--- lua/null-ls-config/init.lua
+++ lua/null-ls-config/init.lua
@@ -1,12 +1,10 @@
 local null_ls = require("null-ls")
 
-local completion = null_ls.builtins.completion
 local formatting = null_ls.builtins.formatting
 
 null_ls.setup({
-    sources = {
-        formatting.stylua,
-        completion.spell,
-        formatting.rustfmt
-    },
+	sources = {
+		formatting.stylua,
+		formatting.rustfmt,
+	},
 })
